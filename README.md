4588b5a9c569207f66399f85c64d7f0ffb8d39d2
derekxkwan <derek.x.kwan@gmail.com>
2017-09-29 18:37:45 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # initial_improv_1
-an improvisational piece
+an improvisational piece for any number of melodic improvisers released under cc by 4.0 (https://creativecommons.org/licenses/by/4.0/legalcode)
+
+kwan_derek-initial_improv_1.pdf - score
+
+
+initial_improv_1_mapper.py - python script to help map letters (run by "python (script name)")
+names are separated by || and numbers denote octaves (starting with 0). can't handle non-ascii characters yet (including diacritical marked letters)
+
+
---
+++ initial_improv_1_mapper.py
@@ -0,0 +1,54 @@
+hypsep = False
+octave = 1
+octmap = 1
+maxletter = 97
+
+def ltrmapper(ltrnum):
+    ltrnum = ltrnum - 97
+    ltroct = int(int(int(ltrnum)/7)/octmap)
+    ltrval = unichr((ltrnum % 7) + 97)
+    ltrstr = str(ltrval) + str(ltroct)
+    return ltrstr
+    
+    
+
+def nameconvert(name):
+    retstr = ""
+    for ltr in name:
+        ltrnum = ord(ltr)
+        if ltrnum < 123 and ltrnum >= 97:
+            retstr = retstr + " " + ltrmapper(ltrnum)
+        else:
+            retstr = retstr + " " + ltr
+    return retstr
+            
+
+person = raw_input("Enter your name: ").strip().lower()
+for ltr in person:
+    ltrnum = ord(ltr)
+    if ltrnum < 123: #is a letter
+        maxletter = max(ltrnum, maxletter)
+        
+names = person.split(" ")
+hypsepstr = raw_input("Hyphenated name as separate? (yes or no): ").strip().lower()
+if hypsepstr[0] == "y":
+    hypsep = True
+octavestr = raw_input("Octaves available: ").strip().lower()
+octave = max(1, min(4, ord(octavestr[0])))
+maxletter = maxletter - 97 #a
+octneeded = round(float(maxletter)/7.0 + 0.5)
+octmap = int(octave/int(octneeded)) + 1
+
+overallstr = ""
+for name in names:
+    if hypsep:
+        for subname in name.split("-"):
+            overallstr = overallstr + " || " + nameconvert(subname)
+    else:
+        overallstr = overallstr + " || " + nameconvert(name)
+
+overallstr = overallstr + " || "
+overallstr = overallstr.strip()
+
+print(overallstr)
+            
